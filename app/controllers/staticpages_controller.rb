f909b569030c85052a0b4c4911c76786b2b4583e
zxum <zion.um@outlook.com>
2020-11-20 09:36:25 -0600

    Add photo feed feature

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,21 @@
+class StaticpagesController < ApplicationController
+  require 'flickr'
+
+  def index 
+    flickr = Flickr.new 
+    username = params[:username]
+    user = flickr.people.findByUsername :username => username
+    userid = user.id 
+
+    if user.nil? 
+      @photos = []
+    else 
+      @photos = flickr.people.getPublicPhotos :user_id => userid
+    end
+    
+  end
+
+  def show
+    
+  end
+end
